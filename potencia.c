b6f28cc3ef4867e49051822b01601eb708266af4
felipefschwade <felipefaeschwade@gmail.com>
2015-10-16 00:03:07 -0300

    Aula 5 Atividade 3 - Alterando a função potência para utilização de ponteiros

--- potencia.c
+++ potencia.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-void potencia(int a, int b){
-	int resultado = 1;
+void potencia(int* resultado,int a, int b){
+	*resultado = 1;
 	for (int i = 0; i < b; ++i){
-		resultado = resultado * a;
+		*resultado = *resultado * a;
 	}
-	printf("%d\n", resultado);
 }
 
 int main(){
-	int a, b;
+	int a, b, resultado;
 	printf("Escreva o número base ");
 	scanf("%d", &a);
 	printf("Escreva o expoente ");
 	scanf("%d", &b);
-	potencia(a, b);
+	potencia(&resultado, a, b);
+	printf("%d\n", resultado);
 }
